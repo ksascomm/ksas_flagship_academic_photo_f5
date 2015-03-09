44f8e88238746979fae4afe931b0f1bc1010fb1c
ksascomm <ksasweb@jhu.edu>
2015-03-09 15:21:49 -0400

    Edited front-page layout; Added Bio & CMDB blog id conditionals

--- front-page.php
+++ front-page.php
@@ -11,15 +11,7 @@ Template Name: Home - Background Photo
 			'orderby'=>'rand',
 			'post_status' => 'publish',
 			'posts_per_page' => '1'));
-		
-
-		if ( false === ( $latest_profile_query = get_transient( 'latest_profile_query' ) ) ) {
-		// It wasn't there, so regenerate the data and save the transient
-		$latest_profile_query = new WP_Query(array(
-			'post_type' => 'profile',
-			'posts_per_page' => 1)); 
-			set_transient( 'latest_profile_query', $latest_profile_query, 2592000 );
-        } 
+			
 		$news_query_cond = $theme_option['flagship_sub_news_query_cond'];
 			if ( false === ( $news_query = get_transient( 'news_mainpage_query' ) ) ) {
 				if ($news_query_cond === 1) {
@@ -33,17 +25,18 @@ Template Name: Home - Background Photo
 								'operator' => 'NOT IN'
 							)
 						),
-						'posts_per_page' => 4)); 
+						'posts_per_page' => 3)); 
 				} else {
 					$news_query = new WP_Query(array(
 						'post_type' => 'post',
-						'posts_per_page' => 4)); 
+						'posts_per_page' => 3)); 
 				}
 			set_transient( 'news_mainpage_query', $news_query, 2592000 );
 			} 	
 
 ?>
 
+<!-- Set photo background -->
 <?php if ( $slider_query->have_posts() ) : while ($slider_query->have_posts()) : $slider_query->the_post(); ?>
 		<div class="row" id="photo">
 			<div class="small-12 columns">
@@ -61,23 +54,21 @@ Template Name: Home - Background Photo
 
 <!-- Begin main content area -->
 <div class="row wrapper radius10" id="page" role="main">
-	<div class="small-12 large-8 columns">
+	<div class="small-12 columns">
 
 	    <?php if ( $news_query->have_posts() ) : ?>
 	    	<div class="row">
 	    		<div class="small-12 columns">
-	    			<div class="panel callout radius10 small-12 columns last">
 	            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	                <h4><?php the_title(); ?></h4>
                     <?php the_content(); ?>
                 <?php endwhile; endif; ?>
-	        </div>
 	    		</div>
 	    	</div>
 	        <div class="row">
 	        <h4><?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
 	           <?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
-	                <div class="small-12 medium-6 columns post-container">
+	                <div class="small-12 large-4 columns post-container">
 	                    <div class="row">
 	                        <div class="small-11 small-centered columns post">
         	                    <a href="<?php the_permalink();?>" title="<?php the_title(); ?>">
@@ -100,9 +91,14 @@ Template Name: Home - Background Photo
 	                </div>
 	           <?php endwhile; ?>
 	        </div>
-	        <div class="row">
-			    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>"><h5 class="black">View All <?php echo $theme_option['flagship_sub_feed_name']; ?></h5></a>
-			</div>
+	        <?php
+			   $blog_id = get_current_blog_id();
+			   if ( 57 != $blog_id && 58 != $blog_id ) { // 57=Biology 58=CMDB Site IDs. Neither have News posts.
+			?>
+		        <div class="row">
+				    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>"><h5 class="black">View All <?php echo $theme_option['flagship_sub_feed_name']; ?></h5></a>
+				</div>
+			<?php } ?>
 		<?php endif; ?>
 	</div>
 	<?php locate_template('parts-sidebar.php', true, false); ?>	
