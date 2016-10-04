61357cfb7681217befa86249d32cf8aa934620ea
ksascomm <ksasweb@jhu.edu>
2016-10-04 14:22:47 -0400

    accessibility edits to links

--- front-page.php
+++ front-page.php
@@ -40,7 +40,6 @@ Template Name: Home - Background Photo
 <?php if ( $slider_query->have_posts() ) : while ($slider_query->have_posts()) : $slider_query->the_post(); ?>
 		<div class="row" id="photo">
 			<div class="small-12 columns">
-
 				<script defer type="text/javascript">
 				    jQuery(document).ready(function() {
 				        jQuery.backstretch(["<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>"], {fade: 750});
@@ -55,7 +54,6 @@ Template Name: Home - Background Photo
 <!-- Begin main content area -->
 <div class="row wrapper radius10" id="page" role="main">
 	<div class="small-12 columns">
-
 	    <?php if ( $news_query->have_posts() ) : ?>
 	    	<div class="row">
 	    		<main class="content small-12 columns">
@@ -66,41 +64,38 @@ Template Name: Home - Background Photo
 	    		</main>
 	    	</div>
 	        <div class="row">
-	        <div class="small-12 columns">
-	        <h4><?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
-	        </div>
-	           <?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
+		        <div class="small-12 columns">
+		        	<h4><?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
+		        </div>
+           		<?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
 	                <div class="small-12 large-4 columns post-container">
 	                    <div class="row">
 	                        <div class="small-11 small-centered columns post">
-        	                    <a href="<?php the_permalink();?>" title="<?php the_title(); ?>">
-            	                    <?php if(has_post_thumbnail()) { ?>
-            	                        <div class="row">
-            	                            <div class="small-12 columns">
-            	                                <?php the_post_thumbnail('rss', array('align'=>'center')); ?>
-            	                            </div>
-            	                        </div>
-                                    <?php } ?>
-                                    <div class="row">
-                                        <div class="small-12 columns">
-                                            <h5><?php the_title(); ?></h5>
-                                            <?php the_excerpt(); ?>
-                                        </div>
+        	                    <?php if(has_post_thumbnail()) { ?>
+        	                        <div class="row">
+        	                            <div class="small-12 columns">
+        	                                <?php the_post_thumbnail('rss', array('align'=>'center')); ?>
+        	                            </div>
+        	                        </div>
+                                <?php } ?>
+                                <div class="row">
+                                    <div class="small-12 columns">
+                                        <h5>
+                                        	<a href="<?php the_permalink();?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                                        </h5>
+                                        <?php the_excerpt(); ?>
                                     </div>
-        	                    </a>
+                                </div>
 	                        </div>
 	                    </div>
 	                </div>
-	           <?php endwhile; ?>
+          		<?php endwhile; ?>
 	        </div>
-	        <?php
-			   $blog_id = get_current_blog_id();
-			   if ( 57 != $blog_id && 58 != $blog_id ) { // 57=Biology 58=CMDB Site IDs. Neither have News posts.
-			?>
-		        <div class="row">
-				    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>"><h5 class="black">View All <?php echo $theme_option['flagship_sub_feed_name']; ?></h5></a>
-				</div>
-			<?php } ?>
+	        <div class="row">
+			    <h5>
+			    	<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">View All <?php echo $theme_option['flagship_sub_feed_name']; ?></a>
+			    </h5>
+			</div>
 		<?php endif; ?>
 	</div>
 	<?php locate_template('parts-sidebar.php', true, false); ?>	
