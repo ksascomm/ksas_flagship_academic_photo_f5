853d94d54b388402062b704e466f6f0bff402f18
ksascomm <ksasweb@jhu.edu>
2016-12-28 13:28:43 -0500

    schema.org markup

--- front-page.php
+++ front-page.php
@@ -56,9 +56,8 @@ Template Name: Home - Background Photo
 	<div class="small-12 columns">
 	    <?php if ( $news_query->have_posts() ) : ?>
 	    	<div class="row">
-	    		<main class="content small-12 columns">
+	    		<main class="content small-12 columns" itemprop="mainEntity" itemscope="itemscope" itemtype="http://schema.org/Blog">
 	            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	                <h1><?php the_title(); ?></h1>
                     <?php the_content(); ?>
                 <?php endwhile; endif; ?>
 	    		</main>
@@ -68,27 +67,30 @@ Template Name: Home - Background Photo
 		        	<h4><?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
 		        </div>
            		<?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
-	                <div class="small-12 large-4 columns post-container">
+	                <article class="small-12 large-4 columns post-container news-item" aria-labelledby="post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
 	                    <div class="row">
 	                        <div class="small-11 small-centered columns post">
         	                    <?php if(has_post_thumbnail()) { ?>
         	                        <div class="row">
         	                            <div class="small-12 columns">
-        	                                <?php the_post_thumbnail('rss'); ?>
+        	                                <?php the_post_thumbnail('rss'); array('class'	=> "floatleft", 'itemprop' => 'image'); ?>
         	                            </div>
         	                        </div>
                                 <?php } ?>
                                 <div class="row">
                                     <div class="small-12 columns">
-                                        <h5>
-                                        	<a href="<?php the_permalink();?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                                        </h5>
-                                        <?php the_excerpt(); ?>
+                                        <h1 itemprop="headline">
+                                        	<a href="<?php the_permalink();?>" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a>
+                                        </h1>
+                                        <meta name="dateModified" itemprop="dateModified" content="<?php the_modified_date(); ?>" />
+                                        <div class="entry-content" itemprop="text">	
+                                       		<?php the_excerpt(); ?>
+                                        </div>
                                     </div>
                                 </div>
 	                        </div>
 	                    </div>
-	                </div>
+	                </article>
           		<?php endwhile; ?>
 	        </div>
 	        <div class="row">
--- header.php
+++ header.php
@@ -36,17 +36,18 @@
 </head>
 <?php $theme_option = flagship_sub_get_global_options(); $color_scheme = $theme_option['flagship_sub_color_scheme']; global $blog_id; $site_id = 'site-' . $blog_id; ?>
 <body <?php body_class($color_scheme . ' ' . $site_id); ?> onLoad="viewport()">
-	<header>
+	<header itemscope="itemscope" itemtype="http://schema.org/WPHeader" role="banner">
 	   <div id="mobile-nav">
 			<div class="row">
 				<div class="small-12 large-4 columns centered">
-					<div class="mobile-logo centered"><a href="<?php echo network_site_url(); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/images/ksas-logo-horizontal.png" alt="jhu logo"></a>
-						<h1><a class="white" href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?></a></h1>
+					<?php if ($blog_id == 79)  : ?>
+						<div class="mobile-logo centered"><a href="http://jhu.edu"><img src="<?php echo get_template_directory_uri() ?>/assets/images/jhu-horizontal.png" alt="jhu logo"></a>
+					<?php else : ?> 
+						<div class="mobile-logo centered"><a href="<?php echo network_site_url(); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/images/ksas-logo-horizontal.png" alt="krieger logo"></a>
+					<?php endif; ?>	
+						<h1 itemprop="headline"><a class="white" href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?></a></h1>
 					</div>
 				</div>
-			</div>
-			<div class="row hide-for-print">
-				<?php get_template_part( '/parts/search-mobile' ); ?>
 			</div>
 		</div>
 		
@@ -58,20 +59,23 @@
 						<li class="logo"><a href="<?php echo network_home_url(); ?>" title="Krieger School of Arts & Sciences"><img src="<?php echo get_template_directory_uri() ?>/assets/images/ksas-logo.png" alt="jhu logo"></a></li>
 					</div>
 					<div class="medium-9 columns">
-						<h1><a class="white" href="<?php echo site_url(); ?>"><span class="small"><?php echo get_bloginfo ( 'description' ); ?></span><?php echo get_bloginfo( 'title' ); ?></a></h1>
+						<h1 itemprop="headline">
+							<a class="white" href="<?php echo site_url(); ?>">
+								<?php if( !empty( get_bloginfo('description') )) : ?>
+										<span class="small" itemprop="description"><?php echo get_bloginfo ( 'description' ); ?></span>
+								<?php endif; ?>
+								<?php echo get_bloginfo( 'title' ); ?>
+							</a>
+						</h1>
 					</div>			
 				</div>
 			</div>
-			<div class="row hide-for-print">
+			<div class="row hide-for-print" aria-label="Main Menu" id="main_nav" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
 				<?php wp_nav_menu( array( 
 					'theme_location' => 'main_nav', 
 					'menu_class' => 'nav-bar', 
-					'container' => 'nav',
-					'container_id' => 'main_nav', 
-					'container_class' => 'small-12 columns',
 					'fallback_cb' => 'foundation_page_menu',
 					'walker' => new foundation_navigation(),
-					'items_wrap' => '<ul id="%1$s" class="%2$s" role="navigation" aria-label="Main menu">%3$s</ul>',
 					'depth' => 2  )); ?> 
 			</div>
 		</div>
